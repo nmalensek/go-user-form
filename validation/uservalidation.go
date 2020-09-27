5185fe2eb642ebe952c36c361d288d4c74ea4900
Nick Malensek <nick@malensek.net>
2020-09-26 20:35:01 -0600

    Added user validation testing and fixed issues along the way

--- validation/uservalidation.go
+++ validation/uservalidation.go
@@ -7,22 +7,26 @@ import (
 	"github.com/nmalensek/go-user-form/model"
 )
 
-const emailPattern = `/^[a-zA-Z0-9.!#$%&'*+/=?^_{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/`
+//EmailPattern is a regular expression that should capture valid email addresses, source:
+//https://html.spec.whatwg.org/multipage/input.html#valid-e-mail-address
+var EmailPattern = regexp.MustCompile(`^[a-zA-Z0-9.!#$%&'*+/=?^_{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$`)
 
 //ValidateInput compares a given user's properties against requirements and returns
 //errors corresponding to the properties not meeting the requirements.
 func ValidateInput(subj model.User) []UserError {
 	errs := make([]UserError, 0)
 	stringProps := []ValItem{
-		ValItem{name: "firstName", val: subj.FirstName},
-		ValItem{name: "lastName", val: subj.LastName},
-		ValItem{name: "email", val: subj.Email},
-		ValItem{name: "organization", val: subj.Organization}}
+		ValItem{name: "FirstName", val: subj.FirstName},
+		ValItem{name: "LastName", val: subj.LastName},
+		ValItem{name: "Email", val: subj.Email},
+		ValItem{name: "Organization", val: subj.Organization}}
 
-	appendErrorIfReq(errs, stringProps)
+	appendErrorIfReq(&errs, stringProps)
 
-	appendErrorIfNoMatch(errs, []ValItem{
-		ValItem{name: "email", val: subj.Email, pattern: emailPattern}})
+	if len(subj.Email) > 0 {
+		appendErrorIfNoMatch(&errs, []ValItem{
+			ValItem{name: "Email", val: subj.Email, pattern: EmailPattern}})
+	}
 
 	return errs
 }
@@ -38,23 +42,35 @@ type UserError struct {
 type ValItem struct {
 	name    string
 	val     string
-	pattern string
+	pattern *regexp.Regexp
 }
 
-func appendErrorIfReq(e []UserError, item []ValItem) {
+//appendErrorIfReq appends an error if an item is missing and is required.
+func appendErrorIfReq(e *[]UserError, item []ValItem) {
 	for _, p := range item {
 		if p.val == "" {
-			uErr := UserError{PropName: p.name, PropValue: p.val, Message: fmt.Sprintf("%v is required.", p.name)}
-			e = append(e, uErr)
+			uErr := UserError{PropName: p.name, PropValue: p.val, Message: RequiredMessage(p.name)}
+			*e = append(*e, uErr)
 		}
 	}
 }
 
-func appendErrorIfNoMatch(e []UserError, item []ValItem) {
+//appendErrorIfNoMatch appends an error if an item does not match the specified regex.
+func appendErrorIfNoMatch(e *[]UserError, item []ValItem) {
 	for _, p := range item {
-		if match, err := regexp.MatchString(p.pattern, p.val); match == false || err != nil {
-			newErr := UserError{PropName: p.name, PropValue: p.val, Message: fmt.Sprintf("%v is not in the correct format.", p.name)}
-			e = append(e, newErr)
+		if !p.pattern.MatchString(p.val) {
+			newErr := UserError{PropName: p.name, PropValue: p.val, Message: IncorrectFormatMessage(p.name)}
+			*e = append(*e, newErr)
 		}
 	}
+}
+
+//IncorrectFormatMessage returns the standard error message for a property that's formatted incorrectly.
+func IncorrectFormatMessage(prop string) string {
+	return fmt.Sprintf("%v is not in the correct format.", prop)
+}
+
+//RequiredMessage returns the standard error message for a property that is required but is missing.
+func RequiredMessage(prop string) string {
+	return fmt.Sprintf("%v is required.", prop)
 }
---
+++ validation/uservalidation_test.go
@@ -0,0 +1,86 @@
+package validation
+
+import (
+	"testing"
+
+	"github.com/nmalensek/go-user-form/model"
+)
+
+func TestValidEmails(t *testing.T) {
+	emails := []string{"test@email.com", "a@bc.de", "hello-world@example.org", "first.last@co.co"}
+
+	for _, v := range emails {
+		if !EmailPattern.MatchString(v) {
+			t.Errorf("Valid email %v did not match the regex.", v)
+		}
+	}
+}
+
+func TestInvalidEmails(t *testing.T) {
+	emails := []string{"", "a@b.", "stringwithoutat", "test,,,@@mail.net"}
+
+	for _, v := range emails {
+		if EmailPattern.MatchString(v) {
+			t.Errorf("Invalid email %v incorrectly matched the regex.", v)
+		}
+	}
+}
+
+func TestValidateInput(t *testing.T) {
+	goodUser := model.User{FirstName: "test1", LastName: "Last",
+		Email: "test@email.com", Organization: "Sales"}
+
+	errors := ValidateInput(goodUser)
+
+	if len(errors) != 0 {
+		for _, e := range errors {
+			t.Errorf("Error occurred when none was expected: %v", e.Message)
+		}
+	}
+}
+
+func TestBadEmail(t *testing.T) {
+	badEmail := model.User{Email: "aaaaa"}
+
+	errors := ValidateInput(badEmail)
+
+	if len(errors) == 0 {
+		t.Errorf("Input bad email address %v but was not caught", badEmail.Email)
+	}
+
+	want := IncorrectFormatMessage("Email")
+	var got string
+	for _, e := range errors {
+		if e.PropName == "Email" {
+			got = e.Message
+			break
+		}
+	}
+	if got != want {
+		t.Errorf("Got %v, wanted %v", got, want)
+	}
+}
+
+func TestIncompleteEntry(t *testing.T) {
+	missingInfo := model.User{FirstName: "test"}
+
+	errors := ValidateInput(missingInfo)
+
+	if len(errors) < 3 {
+		t.Errorf("Information missing from input was not caught.")
+		for _, e := range errors {
+			t.Errorf("Captured error: %v", e.PropName)
+		}
+	}
+
+	names := make(map[string]struct{})
+	names["LastName"] = struct{}{}
+	names["Email"] = struct{}{}
+	names["Organization"] = struct{}{}
+
+	for _, e := range errors {
+		if _, ok := names[e.PropName]; !ok {
+			t.Errorf("%v is missing but no error was created for it.", e.PropName)
+		}
+	}
+}
